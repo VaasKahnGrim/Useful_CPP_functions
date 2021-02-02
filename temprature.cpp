b949573e05ddc5f85cd7ead6a8625daa4a60762e
Vaas_Kahn_Grim <29955667+VaasKahnGrim@users.noreply.github.com>
2021-02-02 07:11:11 -0600

    Update temprature.cpp

--- temprature.cpp
+++ temprature.cpp
@@ -1,32 +1,31 @@
 //Temprature Conversion Functions
 namespace TempConversion{
-
-  //Convert Celcius to Kelvin
+	//Convert Celcius to Kelvin
 	double Celcius2Kelvin(double temp){
 		return temp+273.15;
 	}
 
-  //Convert Celcius to Fahrenheit
+	//Convert Celcius to Fahrenheit
 	double Celcius2Fahrenheit(double temp){
 		return (temp*9/5)+32;
 	}
 
-  //Convert Fahrenheit to Kelvin
+	//Convert Fahrenheit to Kelvin
 	double Fahrenheit2Kelvin(double temp){
 		return ((temp-32)*5/9)+273.15;
 	}
 
-  //Convert Fahrenheit to Celcius
+	//Convert Fahrenheit to Celcius
 	double Fahrenheit2Celcius(double temp){
 		return (temp-32)*5/9;
 	}
 
-  //Convert Kelvin to Celcius
+	//Convert Kelvin to Celcius
 	double Kelvin2Celcius(double temp){
 		return temp-273.15;
 	}
 
-  //Convert Kelvin to Fahrenheit
+	//Convert Kelvin to Fahrenheit
 	double Kelvin2Fahrenheit(double temp){
 		return ((temp-273)*9/5)+32;
 	}
